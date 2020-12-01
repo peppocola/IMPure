eeaff110c766d1200c59cfb39319bc5b159cc623
peppocola <giuseppecolavito999@gmail.com>
2020-12-01 19:07:20 +0100

    Project structure and internal representation structures

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for IMPure
+
+## Unreleased changes
---
+++ app/Grammar.hs
@@ -0,0 +1,47 @@
+-- Grammar.hs contains the structures for the internal representation of
+-- the data. 
+module Grammar where
+
+data Program
+    = Program [Command]
+    deriving Show
+
+data Command
+    = VariableDeclaration String Expression
+    | Assignment String Expression
+    | IfThenElse BExp [Command] [Command]
+    | While BExp [Command]
+    | Skip
+    deriving Show
+
+data Expression
+    = ArithmeticExpression AExp
+    | BooleanExpression BExp
+    deriving Show
+
+data AExp
+    = Constant Int
+    | AVariable String
+    | Add AExp AExp
+    | Sub AExp AExp
+    | Mul AExp AExp
+    deriving Show
+
+data BExp
+    = Boolean Bool
+    | BVariable String
+    | Not BExp
+    | Or BExp BExp
+    | And BExp BExp
+    | Comparison AExp AExp Operator
+    deriving Show
+
+data Operator
+    = Lt | Le | Gt | Ge | Eq | Neq
+instance Show Operator where
+    show Lt = "<"
+    show Le = "<="
+    show Gt = ">"
+    show Ge = ">="
+    show Eq = "=="
+    show Neq = "!="
